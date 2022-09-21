8caf11c302a618dc1289d7c66f57b0447c0f6ecd
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-09-21 20:46:09 +0900

    Nav 80%

---
+++ src/common/NavBar/index.globalstyle.jsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components"
+export default createGlobalStyle`
+    a {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-decoration: none;
+    }
+`
--- src/common/NavBar/index.jsx
+++ src/common/NavBar/index.jsx
@@ -1,10 +1,51 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import GlobalStyle from './index.globalstyle'
+import logo from './navlogo.png'
 import * as S from './index.style'
 const Render = () => {
+    const [logohover, setLogohover] = useState(false)
+    const contents = [
+        { title: '운동 소개', param: '/introduce' },
+        { title: '추천', param: '/recommend' },
+        { title: '기록', param: '/record' },
+    ]
     return (
         <>
-            <S.Main>
-            </S.Main>
-            <S.dropdownMenu/>
+            <GlobalStyle/>
+            <S.background>
+            <S.logoContainer>
+                <div>
+                    <Link to='/'>
+                        <S.Logo
+                        src={logo}
+                        onMouseEnter={() => {setLogohover(true)}}
+                        onMouseLeave={() => {setLogohover(false)}}
+                        />
+                    </Link>
+                </div>
+                <S.logoTitle isvisible={logohover}>어깨어때</S.logoTitle>
+            </S.logoContainer>
+            <S.navigator>
+                {
+                    contents.map((item) => {
+                        return (
+                            <S.container>
+                                <Link to={item.param}>
+                                    <S.menu>{item.title}</S.menu>
+                                </Link>
+                            </S.container>
+                        )
+                    })
+                }
+            </S.navigator>
+            <S.loginForm>
+                
+                <S.loginText>로그인</S.loginText>
+                &nbsp;
+                <S.loginText>회원가입</S.loginText>
+            </S.loginForm>
+            </S.background>
         </>
     )
 }
