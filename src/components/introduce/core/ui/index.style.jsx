829716b1e9471fb2ec3e356759aeb87c4483ce80
kim geon ho <80884472+qfourca@users.noreply.github.com>
2022-09-30 10:23:00 +0900

    use app

--- src/components/introduce/core/ui/index.style.jsx
+++ src/components/introduce/core/ui/index.style.jsx
@@ -11,7 +11,7 @@ export const Main = styled.div`
     opacity: ${({hide}) => hide ? 0 : 1};
 `
 export const ProgressBarContainer = styled.div`
-    width: 97%;
+    width: 96%;
     height: 6px;
     position: absolute;
     background-color:#999;
@@ -37,62 +37,3 @@ export const ProgressBar = styled.div`
         z-index: 0;
     }
 `
-export const ProgressBall = styled.div`
-
-`
-
-//     .MomentBall {
-//         width: $diameter;
-//         height: $diameter;
-//         border-radius: 50%;
-//         background-color: green;
-//         position: absolute;
-//         bottom: -3px;
-//         cursor: pointer;
-//     }
-//     .ProgressBar {
-
-//         .ProgressBall {
-
-//         }
-//     }
-// }
-// .FunctionContainer {
-//     width: 100%;
-//     height: 50px;
-//     position: absolute;
-//     bottom: 0;
-//     display: flex;
-//     justify-content: space-around;
-//     align-items: center;
-//     display: flex;
-//     .Functions {
-//         width: 96%;
-//         height: 50px;
-//         display: flex;
-//         align-items: center;
-//         gap: 5px;
-//         .Function {
-//             width: 30px;
-//             height: 30px;
-//             background-repeat : no-repeat;
-//             background-size : cover;
-//             cursor: pointer;
-//         }
-//         .TimeProgress {
-//             margin-left: 10px;
-//             font-size: 20px;
-//             font-family: "Roboto";
-//             color: #eee;
-//         }
-//     }
-//     .left {
-//         width: 60%;
-//         justify-content: flex-start;
-//     }
-//     .right {
-//         width: 30%;
-//         justify-content: flex-end;
-//     }
-// }
-// }
