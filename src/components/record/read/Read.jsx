e46b24617e4078ca4c53829692d79c231f9df25f
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-10-04 17:23:15 +0900

    [UPDATE] UI with Img balance

--- src/components/record/read/Read.jsx
+++ src/components/record/read/Read.jsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import * as R from "./Read.style"
-import dumImg from "../testimgs/220804.jpg"
+import React from "react";
+import * as R from "./Read.style";
+// import dumImg from "../testimgs/220804.jpg";
+import dumImg from "../testimgs/220803.jpg";
 
 const Read = () => {
-    // 이미지, 운동 종류, 운동 부위, 글
-    const post = {
-        title:"밥 먹기",
-        content:"밥을 어떻게 먹나요?"
-    }
+  // 이미지, 운동 종류, 운동 부위, 글
+  const post = {
+    img: dumImg,
+    exe: "밥 먹기",
+    part: ["팔", "입"],
+    content: "밥먹기 운동을 했따",
+  };
 
-    return (
-        <R.Wrapper>
-            <R.Content>
-                <R.Header>
+  return (
+    <R.Wrapper>
+      <R.Section>
+        <R.ImgWrapper>
+          <R.Img src={post.img} />
+        </R.ImgWrapper>
+        <R.Article></R.Article>
+
+        {/* <R.Header>
                     <R.Title>{post.title}</R.Title>
                 </R.Header>
                 <R.Article>
-                    
-                </R.Article>
-            </R.Content>
-        </R.Wrapper>
-    );
+                
+                </R.Article> */}
+      </R.Section>
+    </R.Wrapper>
+  );
 };
 
 export default Read;
--- src/components/record/read/Read.style.js
+++ src/components/record/read/Read.style.js
@@ -6,21 +6,30 @@ export const Wrapper = styled.div`
     display:flex;
     justify-content:center;
 `
-export const Content = styled.section`
-    width:70%;
-    @media (max-width:800px) {
-        width:100%;
-    }
-    height:100%;
-    background-color:white;
-`
-export const Header = styled.header`
-    margin-top:50px;
-`
-export const Title = styled.h1`
-    font-size:50px;
+
+export const Section = styled.section`
+    margin:40px 30px;
+    display:flex;
+    height:fit-content;
+    max-width:935px;
+    min-height:450px;
+    max-height:600px;
 `
 
+export const ImgWrapper = styled.div`
+    display:flex;
+    justify-content:center;
+    align-content:center;
+`
+export const Img = styled.img`
+    object-fit:contain;
+    max-height:600px;
+    max-width:600px;
+`
+
+
 export const Article = styled.article`
-    
+    width:335px;
+    height:auto;
+    background-color:powderblue;
 `
