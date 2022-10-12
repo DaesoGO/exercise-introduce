58f28a2658f6f50dfde7c447bcbb84e77cca541e
오은지 <81677536+oheunji05@users.noreply.github.com>
2022-10-12 22:12:18 +0900

    nav

--- src/common/NavBar/index.jsx
+++ src/common/NavBar/index.jsx
@@ -8,7 +8,7 @@ const Render = () => {
         return 0;
     }
     const contents = [
-        { title: '운동 소개', param: '/introduce' },
+        { title: '운동', param: '/introduce' },
         { title: '추천', param: '/recommend' },
         { title: '기록', param: '/record' },
     ]
@@ -16,24 +16,26 @@ const Render = () => {
         <>
             <GlobalStyle/>
             <S.background>
-                <S.logoContainer>
-                    <S.imgContainer to='/'>
-                        <S.Logo src={logo}/>
-                    </S.imgContainer>
-                </S.logoContainer>
-                <S.navigator>
-                    {
-                        contents.map((item, idx) => {
-                            return (
-                                <S.container key={idx} onClick={navToggling}>
-                                    <Link to={item.param}>
-                                        <S.menu>{item.title}</S.menu>
-                                    </Link>
-                                </S.container>
-                            )
-                        })
-                    }
-                </S.navigator>
+                <S.leftContainer>
+                    <S.logoContainer>
+                        <S.imgContainer to='/'>
+                            <S.Logo src={logo}/>
+                        </S.imgContainer>
+                    </S.logoContainer>
+                    <S.navigator>
+                        {
+                            contents.map((item, idx) => {
+                                return (
+                                    <S.container key={idx} onClick={navToggling}>
+                                        <Link to={item.param}>
+                                            <S.menu>{item.title}</S.menu>
+                                        </Link>
+                                    </S.container>
+                                )
+                            })
+                        }
+                    </S.navigator>
+                </S.leftContainer>
                 <S.loginForm>
                     <Link to={'/login'}>
                         <S.loginText>로그인</S.loginText>
