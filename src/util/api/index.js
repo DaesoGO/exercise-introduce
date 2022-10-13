468072d4486490cef3c546904d992aa318cbf132
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-10-13 20:20:29 +0900

    [API] get exeList [TRY] remove

--- src/util/api/index.js
+++ src/util/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // const token = () => localStorage.getItem('access_token')
 
 const api = axios.create({
-  baseURL: 'http://10.80.162.42:8000/api',
+  baseURL: 'http://10.80.161.195:8000/api',
   headers: {Authorization:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiJ0b21hdG80MTE2IiwiaWF0IjoxNjY1NTYzODg1LCJleHAiOjE3MDg3NjM4ODUsImlzcyI6ImJsYW5rIiwic3ViIjoiYWNjZXNzVG9rZW4ifQ.q0lZa3it0ZvB-yUulVk4i5XQ9w5LdK-TbH1mUOQYVOw"}
 //   headers: localStorage.getItem('access_token') && {
 //     Authorization: `Bearer ${token()}`
