6d0ecc214bd20c6148d900ee3bf44e4c37993e1c
오은지 <81677536+oheunji05@users.noreply.github.com>
2022-11-02 20:37:03 +0900

    [update] UI

--- src/components/recommend/exercise.jsx
+++ src/components/recommend/exercise.jsx
@@ -2,31 +2,72 @@ import * as S from './exercise.style'
 import logo from './logo.png'
 
 import Dumbbell from '../../static/recommend/exercise/dumbbell.png'
+import Hulahoop from '../../static/recommend/exercise/hulahoop.png'
+import Jumpping from '../../static/recommend/exercise/jumpping.png'
+import Pushup from '../../static/recommend/exercise/pushup.png'
+import RunningMachine from '../../static/recommend/exercise/runningmachine.png'
+import SitUP from '../../static/recommend/exercise/sit_up.png'
 
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const dummy = [
-    {
-        src: Dumbbell,
-        name: "팔운동"
-    }
-]
+const Render = ({
+    exercises
+}) => {
 
-const Render = () => {
-
-    let [name, setName] = useState()
-
-    // useEffect(()=>{
-    //     axios.get('')
-    //     .then((result)=>{console.log(result)})
-    // })
-
+    let [img, setImg] = useState()
+    let [title, setTitle] = useState()
+    let [muscle, setMuscle] = useState()
+    let [difficulty, setDifficulty] = useState()
+    let [explanation, setExplanation] = useState()
+    let [precautions, setPrecautions] = useState()
+    let [videoFile, setVideoFile] = useState()
+    console.log(exercises)
     return <S.Main>
-        {/* <img src={logo} width='350px' height='350px'></img> */}
-        <S.Img>
-            <img src={Dumbbell} width='250px' height='250px'></img>
-        </S.Img>
+        <S.LeftExercise>
+            {
+                exercises.length >= 2 ?
+                <>
+                <Content
+                    img={Dumbbell}
+                />
+                    <S.ContentWrapper>
+                    <span>{exercises[0].title}</span>
+                    <S.Text>{exercises[0].explanation}</S.Text>
+                    </S.ContentWrapper>
+                </> : <></>
+            }
+        </S.LeftExercise>
+
+        <S.RightExercise>
+            {
+                exercises.length >= 2 ?
+                <>
+                    <Content
+                        img={Hulahoop}
+                    />
+                    <S.ContentWrapper>
+                    <span>{exercises[1].title}</span>
+                    <S.Text>{exercises[1].explanation}
+                    </S.Text>
+                    <S.Temp></S.Temp>
+                    </S.ContentWrapper>
+                </>
+                : 
+                <></>
+            }
+
+        </S.RightExercise>
     </S.Main>
 }
+
+const Content = ({
+    img
+}) => {
+    console.log(img)
+    return <S.ExerciseContent>
+        <img src={img}></img>
+    </S.ExerciseContent>
+}
+
 export default Render
--- src/util/api/index.js
+++ src/util/api/index.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://10.80.161.250:8000/api",
+  // 로그인 했을 때 서버통신
+  baseURL: "http://10.80.161.189:8000/api",
+  // baseURL: "http://10.80.161.250:8000/api",
   timeout: 1000,
 });
 
 api.interceptors.request.use(
   function (config) {
+    // 로그인 되있을 때 access_token
     const accessToken = localStorage.getItem("access_token");
     if (accessToken) {
       config.headers = {
